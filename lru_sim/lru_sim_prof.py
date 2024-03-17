31a30e9dde73b81efa06e79b9475e500c690734f
Eunji Lee <eunjicious@gmail.com>
2024-03-17 18:20:16 +0900

    lru_sim

---
+++ lru_sim/lru_sim_prof.py
@@ -0,0 +1,28 @@
+
+
+class CacheSimulator:
+    def __init__(self, cache_slots):
+        self.cache_slots = cache_slots
+        self.cache_hit = 0
+        self.tot_cnt = 0
+    
+    def do_sim(self, page):
+        print("do_sim")
+        # Do programming here! 
+        
+    def print_stats(self):
+        print("cache_slot = ", self.cache_slots, "cache_hit = ", self.cache_hit, "hit ratio = ", self.cache_hit / self.tot_cnt)
+
+
+if __name__ == "__main__":
+
+    data_file = open("./linkbench.trc")
+    lines = data_file.readlines()
+    for cache_slots in range(100, 1001, 100):
+        cache_sim = CacheSimulator(cache_slots)
+        for line in lines:
+            page = line.split()[0]
+            cache_sim.do_sim(page)
+        
+        cache_sim.print_stats()
+
