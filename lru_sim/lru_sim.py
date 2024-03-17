3b197b50a12cea808a5c6551718ed6a60101ce79
Eunji Lee <eunjicious@gmail.com>
2024-03-17 18:20:50 +0900

    lru_sim updated

--- lru_sim/lru_sim.py
+++ lru_sim/lru_sim.py
@@ -1,18 +1,28 @@
 
 
-def lru_sim(cache_slots):
-  cache_hit = 0
-  tot_cnt = 0
+class CacheSimulator:
+    def __init__(self, cache_slots):
+        self.cache_slots = cache_slots
+        self.cache_hit = 0
+        self.tot_cnt = 0
+    
+    def do_sim(self, page):
+        print("do_sim")
+        # Do programming here! 
+        
+    def print_stats(self):
+        print("cache_slot = ", self.cache_slots, "cache_hit = ", self.cache_hit, "hit ratio = ", self.cache_hit / self.tot_cnt)
 
-  data_file = open("linkbench.trc")
-
-  for line in data_file.readlines():
-    lpn = line.split()[0]
-
-    # Program here 
-
-    #print("cache_slot = ", cache_slots, "cache_hit = ", cache_hit, "hit ratio = ", cache_hit / tot_cnt)
 
 if __name__ == "__main__":
-  for cache_slots in range(100, 1000, 100):
-    lru_sim(cache_slots)
+
+    data_file = open("./linkbench.trc")
+    lines = data_file.readlines()
+    for cache_slots in range(100, 1001, 100):
+        cache_sim = CacheSimulator(cache_slots)
+        for line in lines:
+            page = line.split()[0]
+            cache_sim.do_sim(page)
+        
+        cache_sim.print_stats()
+
